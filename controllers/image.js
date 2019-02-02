a7157ea641a4e9a7a8fd18a4fc03ae92f1ef1817
Jerry Yu <jr2yu@uwaterloo.ca>
2019-02-02 00:44:20 -0500

    api

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai');
 
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: 'YOUR_API_KEY_HERE'
+ apiKey: 'd13bd6f3623944e98e3f9bfbfd6f5437'
 });
 
 const handleApiCall = (req, res) => {
